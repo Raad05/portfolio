ec6e91f1ab9630e115975e311f28012e302474d4
Raad05 <ashabulraad@gmail.com>
2024-02-27 21:39:47 +0600

    intro complete

--- app/components/Intro/Intro.tsx
+++ app/components/Intro/Intro.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import stack from "../../../public/stack.png";
+import stack from "../../../public/stack.svg";
 import {
   FaFacebookSquare,
   FaTwitter,
@@ -17,7 +17,7 @@ import { MdContacts } from "react-icons/md";
 
 const Intro = () => {
   return (
-    <div className="intro container mx-auto flex items-center mt-10 pb-60 min-h-screen">
+    <div className="intro container mx-auto flex mt-10 pb-60 min-h-screen">
       <div className="w-1/2 mr-10">
         <div className="who-am-i">
           <div className="text-5xl font-bold">
@@ -46,7 +46,7 @@ const Intro = () => {
             >
               This is{" "}
               <motion.span
-                className="text-blue-400"
+                className="text-blue-400 glow-name"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 0.75, ease: "easeInOut", delay: 1 }}
@@ -111,33 +111,79 @@ const Intro = () => {
             </li>
           </ul>
         </motion.div>
-        {/* <motion.div
-          className="socials my-10"
+      </div>
+      <motion.div
+        className="w-1/2 ml-10"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.5, ease: "easeInOut" }}
+      >
+        <motion.div
+          animate={{ y: [0, 15, 0] }}
+          transition={{
+            duration: 3,
+            ease: "easeInOut",
+            repeat: Infinity,
+          }}
+        >
+          <Image
+            src={stack}
+            alt="me"
+            className="my-16 rounded-3xl glow-img"
+          ></Image>
+        </motion.div>
+        <motion.div
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.75, ease: "easeInOut", delay: 0.75 }}
+          transition={{ duration: 1, delay: 1, ease: "easeInOut" }}
         >
-          <h4 className="text-2xl my-5 font-bold">Connect with me:</h4>
-          <div className="flex">
+          <Link
+            href="/"
+            className="btn mr-3 hover:bg-red-600/20 border-2 hover:scale-110 border-green-600 hover:border-red-600 rounded-full text-lg"
+          >
+            <MdContacts size={20}></MdContacts>Contact
+          </Link>
+          <Link
+            href="/"
+            className="btn ml-3 hover:bg-purple-600/20 border-2 hover:scale-110 border-blue-600 hover:border-purple-600 rounded-full text-lg"
+          >
+            <TfiDownload size={20}></TfiDownload>Resume
+          </Link>
+        </motion.div>
+        <div className="socials my-10">
+          <motion.h4
+            className="text-2xl my-5 font-bold"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.75, ease: "easeInOut", delay: 0.75 }}
+          >
+            Connect with me:
+          </motion.h4>
+          <motion.div
+            className="flex"
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.75, ease: "easeInOut", delay: 1.25 }}
+          >
             <Link
               href="https://www.facebook.com/Ashabul.Yamin.L/"
               target="_blank"
             >
               <FaFacebookSquare
-                className="mx-5 hover:scale-125 duration-200 ease-in-out text-blue-600 bg-white rounded-lg"
-                size={50}
+                className="mr-5 hover:scale-125 duration-200 ease-in-out text-blue-600 bg-white rounded-lg"
+                size={45}
               ></FaFacebookSquare>
             </Link>
             <Link href="https://www.linkedin.com/in/yaminraad/" target="_blank">
               <FaLinkedin
                 className="mx-5 hover:scale-125 duration-200 ease-in-out text-blue-900 bg-white rounded-lg"
-                size={50}
+                size={45}
               ></FaLinkedin>
             </Link>
             <Link href="https://github.com/Raad05" target="_blank">
               <FaGithub
                 className="mx-5 hover:scale-125 duration-200 ease-in-out"
-                size={50}
+                size={45}
               ></FaGithub>
             </Link>
             <Link
@@ -146,40 +192,18 @@ const Intro = () => {
             >
               <FaDiscord
                 className="mx-5 hover:scale-125 duration-200 ease-in-out text-indigo-700"
-                size={50}
+                size={45}
               ></FaDiscord>
             </Link>
             <Link href="https://twitter.com/YaminRaad" target="_blank">
               <FaTwitter
                 className="mx-5 hover:scale-125 duration-200 ease-in-out text-blue-400"
-                size={50}
+                size={45}
               ></FaTwitter>
             </Link>
-          </div>
-        </motion.div> */}
-      </div>
-      <div className="w-1/2 ml-10">
-        <Image
-          src={stack}
-          quality={100}
-          alt="me"
-          className="my-10 rounded-lg"
-        ></Image>
-        <div className="mx-5">
-          <Link
-            href="/"
-            className="btn mx-2 hover:bg-red-600/20 border-2 border-green-600 hover:border-red-600 rounded-full text-lg"
-          >
-            <MdContacts size={20}></MdContacts>Contact
-          </Link>
-          <Link
-            href="/"
-            className="btn mx-2 hover:bg-purple-600/20 border-2 border-blue-600 hover:border-purple-600 rounded-full text-lg"
-          >
-            <TfiDownload size={20}></TfiDownload>Resume
-          </Link>
+          </motion.div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
