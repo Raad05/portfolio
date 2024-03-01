e1ecf3ad82067aaa2fae61814bd5bd149dcf02aa
Raad05 <ashabulraad@gmail.com>
2024-03-01 18:18:57 +0600

    portfolio complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import HomePage from "./homePage/page";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    });
+
+    return () => clearTimeout(timeout);
+  }, []);
   return (
     <main className="main">
-      <HomePage></HomePage>
+      {loading ? (
+        <div className="h-screen">
+          <div className="flex justify-center mt-80 glow-load rounded-full w-10 mx-auto">
+            <span className="loading loading-ring loading-lg"></span>
+          </div>
+        </div>
+      ) : (
+        <HomePage></HomePage>
+      )}
     </main>
   );
 }
