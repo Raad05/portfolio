80c189a321841b6927a5871ea3d7d93cc5d8bebb
Raad05 <ashabulraad@gmail.com>
2024-06-22 00:05:46 +0600

    added speed insights feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const robotoMono = Roboto_Mono({
   subsets: ["latin"],
@@ -27,6 +28,7 @@ export default function RootLayout({
         {children}
         <Footer></Footer>
         <Analytics></Analytics>
+        <SpeedInsights></SpeedInsights>
       </body>
     </html>
   );
