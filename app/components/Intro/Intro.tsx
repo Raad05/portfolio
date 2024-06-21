b1495d8274cae998ac30fb155b326885b876245a
Raad05 <ashabulraad@gmail.com>
2024-06-21 23:40:12 +0600

    resume updated

--- app/components/Intro/Intro.tsx
+++ app/components/Intro/Intro.tsx
@@ -195,7 +195,7 @@ const Intro = () => {
             <MdContacts size={20}></MdContacts>Contact
           </button>
           <Link
-            href="https://drive.google.com/file/d/19Yf5WD4dvniVFkjihZkxceClwQ4Ny0FC/view?usp=sharing"
+            href="https://drive.google.com/file/d/1JrA48Xdz69_6Wix8SZ4X_e1SD4OqZtx3/view?usp=sharing"
             target="_blank"
             className="btn ml-1 md:ml-3 hover:bg-purple-600/20 border-2 hover:scale-110 border-blue-600 hover:border-purple-600 rounded-full text-lg"
           >
